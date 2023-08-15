c79d49d4450d90fb8c978430143243e38fdcee9d
serg <sergeusprecious@gmail.com>
2023-08-15 22:03:01 +0300

    fix:minor

--- app/models.py
+++ app/models.py
@@ -5,9 +5,7 @@ from database import db
 
 class Task(db.Model):
     __tablename__ = "task"
-    taskid = db.Column(
-        db.String(36), primary_key=True, unique=True, default=str(uuid4())
-    )
+    taskid = db.Column(db.String(36), primary_key=True, default=str(uuid4()))
     listid = db.Column(
         db.Integer, db.ForeignKey("list.listid"), nullable=False
     )  # noqa: E501
@@ -17,9 +15,7 @@ class Task(db.Model):
 
 class List(db.Model):
     __tablename__ = "list"
-    listid = db.Column(
-        db.String(36), primary_key=True, unique=True, default=str(uuid4())
-    )
+    listid = db.Column(db.String(36), primary_key=True, default=str(uuid4()))
     url = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(200))
     listids = db.relationship("Task", backref="task")
