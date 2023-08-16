fa54aaa0d3189a4523ebf1da2093676b864d0b11
serg <sergeusprecious@gmail.com>
2023-08-16 22:25:46 +0300

    fix:refactor

--- app/domain/views.py
+++ app/domain/views.py
@@ -24,23 +24,16 @@ def search_list():
 def new_list():
     search_form = forms.SearchList()
     new_list_form = forms.CreateNewList()
-    new_list_created = False
-    new_list_url = None
     if search_form.validate_on_submit():
         return redirect(
             url_for("tasks.show_list", list_url=search_form.list_id.data)
         )  # noqa: E501
     if new_list_form.validate_on_submit():
         new_list_url = str(uuid4())
-        list_urls = db.session.query(List).with_entities(List.url)
-        # If random listid (url) is already in the table, we should roll again
-        while new_list_url in list_urls:
-            new_list_url = str(uuid4())
         new_list_name = f'My to-do list {datetime.datetime.now().strftime("%d.%m.%y")}'  # noqa: E501
         new_list = List(url=new_list_url, name=new_list_name)
         db.session.add(new_list)
         db.session.commit()
-        # Retrieving list id #
         new_list_id = (
             db.session.query(List)
             .with_entities(List.listid)
@@ -62,8 +55,7 @@ def new_list():
         "index.html",
         search_form=search_form,
         new_list_form=new_list_form,
-        new_list_created=new_list_created,
-        new_list_id=new_list_url,
+        new_list_created=False,
     )
 
 
@@ -80,35 +72,25 @@ def show_list(list_url, new_list_created=False):
     cur_list = (
         db.session.query(Task)
         .join(List, Task.listid == List.listid)
-        .add_columns(Task.taskid)
-        .filter(List.url == list_url)
-        .first()
-    )
-    if search_form.validate_on_submit():
-        return redirect(
-            url_for("tasks.show_list", list_url=search_form.list_id.data)
-        )  # noqa: E501
-    # If such list does not exist
-    if cur_list is None:
-        return redirect(url_for("tasks.new_list"))
-    cur_list = (
-        db.session.query(Task)
-        .join(List, Task.listid == List.listid)
         .add_columns(
             Task.taskid,
             Task.description,
             Task.done,
             List.name,
             List.listid,
-            List.url,  # noqa: E501
+            List.url,
         )
         .filter(List.url == list_url)
         .order_by(Task.taskid.desc())
     )
-    db.session.commit()
-    # these entries will be shown in edit_task form in html
+    if cur_list.first() is None:
+        return redirect(url_for("tasks.new_list"))
     tasks_entries = [task.description for task in cur_list]
     edit_task = forms.EditTask(description=tasks_entries)
+    if search_form.validate_on_submit():
+        return redirect(
+            url_for("tasks.show_list", list_url=search_form.list_id.data)
+        )  # noqa: E501
     if edit_task.submit5.data and edit_task.validate():
         new_descriptions = edit_task.description.data
         tasks_ids = [task.taskid for task in cur_list]
@@ -147,11 +129,9 @@ def show_list(list_url, new_list_created=False):
         )
         db.session.add(new_task)
         db.session.commit()
-        # Erasing task_form
         add_task_form.description.data = ""
         return redirect(url_for("tasks.show_list", list_url=cur_list[0].url))
 
-    # If it is a new list
     if request.args.get("new_list_created") == "True":
         return render_template(
             "list.html",
@@ -190,11 +170,9 @@ def delete():
         .filter(List.url == list_url)
         .first()
     )
-    # Check if list is empty
     if list_to_check is not None:
         return redirect(url_for("tasks.show_list", list_url=list_url))
     else:
-        # Delete list
         list_to_delete = (
             db.session.query(List).filter(List.url == list_url).first()
         )  # noqa: E501
